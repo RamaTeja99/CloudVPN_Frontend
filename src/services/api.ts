422654c7ef967a157e8e807ce92f6fbb1781e62c
Rama Teja Batchu <144083564+RamaTeja99@users.noreply.github.com>
2025-08-22 21:59:25 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = "https://cloudvpn-backend.onrender.com/api";
 
 // Automatically attach JWT
 axios.interceptors.request.use(cfg => {
