d9ca0567eb6fc541860315ba1083d89d9459add0
Rama Teja Batchu <144083564+RamaTeja99@users.noreply.github.com>
2025-08-22 21:39:22 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const API_URL = "https://cloudvpnbackend-production.up.railway.app/api";
+const API_URL =  process.env.REACT_APP_API_URL;
 
 // Automatically attach JWT
 axios.interceptors.request.use(cfg => {
